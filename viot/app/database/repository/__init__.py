d9b7b938c7397258df920af7a934d8e3debdb0f8
vuxmai <tuanvumaihuynh@gmail.com>
2024-09-25 10:52:14 +0700

    chore: move async session to base class

--- viot/app/database/repository/__init__.py
+++ viot/app/database/repository/__init__.py
@@ -1,7 +1,8 @@
-from .crud import CrudRepository, ICrudRepository
+from .crud import AsyncSqlalchemyRepository, CrudRepository, ICrudRepository
 from .pagination import Filter, Page, Pageable, PageableRepository, Sort
 
 __all__ = [
+    "AsyncSqlalchemyRepository",
     "CrudRepository",
     "ICrudRepository",
     "PageableRepository",
