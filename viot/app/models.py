29155f052c8064928daa2b7e8e83f50072e85fcb
vuxmai <tuanvumaihuynh@gmail.com>
2024-09-26 10:43:10 +0700

    chore: update role permission model

--- viot/app/models.py
+++ viot/app/models.py
@@ -1,15 +1,21 @@
 from app.module.auth.model.password_reset import PasswordReset
+from app.module.auth.model.permission import Permission
 from app.module.auth.model.refresh_token import RefreshToken
+from app.module.auth.model.role import Role
+from app.module.auth.model.role_permission import RolePermission
 from app.module.auth.model.user import User
+from app.module.auth.model.user_team_role import UserTeamRole
 from app.module.team.model.team import Team
 from app.module.team.model.team_invitation import TeamInvitation
-from app.module.team.model.user_team import UserTeam
 
 __all__ = [
     "User",
     "PasswordReset",
     "Team",
-    "UserTeam",
+    "Role",
+    "Permission",
+    "RolePermission",
+    "UserTeamRole",
     "RefreshToken",
     "TeamInvitation",
 ]
