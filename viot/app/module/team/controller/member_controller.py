b90ea1f5f18119ed372a541924a7a923d4e7fdc4
vuxmai <tuanvumaihuynh@gmail.com>
2024-09-27 16:39:37 +0700

    feat(api): add team member read permission to get paging members

--- viot/app/module/team/controller/member_controller.py
+++ viot/app/module/team/controller/member_controller.py
@@ -11,16 +11,16 @@ from app.common.dto.types import PageQuery, PageSizeQuery
 from app.common.fastapi.serializer import JSONResponse
 from app.database.dependency import DependSession
 from app.database.repository.pagination import SortDirection
+from app.module.auth.dependency import RequireTeamPermission
+from app.module.auth.permission import TeamMemberPermission
 
-from ..constants import TeamRole
-from ..dependency import RequireAnyTeamRole
 from ..dto.member_dto import MemberPagingDto
 from ..service.member_service import MemberService
 
 
 class MemberController(Controller):
     @inject
-    def __init__(self, member_service: MemberService):
+    def __init__(self, member_service: MemberService) -> None:
         super().__init__(
             prefix="/teams/{team_id}/members", tags=["Team Members"], dependencies=[DependSession]
         )
@@ -31,7 +31,7 @@ class MemberController(Controller):
         summary="Get paging members",
         status_code=200,
         responses={200: {"model": MemberPagingDto}},
-        dependencies=[RequireAnyTeamRole({TeamRole.OWNER, TeamRole.ADMIN, TeamRole.MEMBER})],
+        dependencies=[RequireTeamPermission(TeamMemberPermission.READ)],
     )
     async def get_paging_members(
         self,
