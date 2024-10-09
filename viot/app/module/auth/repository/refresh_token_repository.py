bae2e30b99e2ebdab9122cf28061baa87bd6c0df
vuxmai <tuanvumaihuynh@gmail.com>
2024-10-09 10:19:56 +0700

    fix (refresh token): only allow one device login

--- viot/app/module/auth/repository/refresh_token_repository.py
+++ viot/app/module/auth/repository/refresh_token_repository.py
@@ -12,15 +12,6 @@ class RefreshTokenRepository(CrudRepository[RefreshToken, int]):
         stmt = select(RefreshToken).where(RefreshToken.token == token)
         return (await self.session.execute(stmt)).scalar_one_or_none()
 
-    async def find_latest_token_by_user_id(self, user_id: UUID) -> str | None:
-        stmt = (
-            select(RefreshToken.token)
-            .where(RefreshToken.user_id == user_id)
-            .order_by(RefreshToken.expires_at.desc())
-            .limit(1)
-        )
-        return (await self.session.execute(stmt)).scalar_one_or_none()
-
     async def update_token_expires_at(self, token: str, expires_at: datetime) -> None:
         stmt = update(RefreshToken).where(RefreshToken.token == token).values(expires_at=expires_at)
         await self.session.execute(stmt)
--- viot/tests/module/auth/service/test_token_service.py
+++ viot/tests/module/auth/service/test_token_service.py
@@ -3,13 +3,8 @@ from unittest.mock import AsyncMock, Mock
 
 import pytest
 
-from app.module.auth.exception.auth_exception import (
-    InvalidCredentialsException,
-)
-from app.module.auth.exception.token_exception import (
-    InvalidRefreshTokenException,
-    TokenExpiredException,
-)
+from app.module.auth.exception.auth_exception import InvalidCredentialsException
+from app.module.auth.exception.token_exception import InvalidRefreshTokenException
 from app.module.auth.service.token_service import TokenService
 
 
@@ -24,9 +19,7 @@ async def test_renew_token_correctly(
     mock_refresh_token_repository.find_by_token.return_value = mock_refresh_token
     mock_user_repository.find.return_value = mock_user
     mock_refresh_token_repository.save.return_value = mock_refresh_token
-    mock_refresh_token_repository.find_latest_token_by_user_id.return_value = (
-        mock_refresh_token.token
-    )
+
     # when
     result = await token_service.renew_token(refresh_token=mock_refresh_token.token)
 
@@ -35,17 +28,10 @@ async def test_renew_token_correctly(
     assert result.refresh_token is not None
     assert result.access_token_expires_at is not None
 
-    mock_refresh_token_repository.find_by_token.assert_called_once()
-    mock_user_repository.find.assert_called_once()
-    mock_refresh_token_repository.find_latest_token_by_user_id.assert_called_once()
-    mock_refresh_token_repository.update_all_tokens_expires_at.assert_not_called()
-    mock_refresh_token_repository.save.assert_called_once()
-
 
 async def test_renew_token_raises_when_refresh_token_not_found(
     token_service: TokenService,
     mock_refresh_token_repository: AsyncMock,
-    mock_user_repository: AsyncMock,
 ) -> None:
     # given
     mock_refresh_token_repository.find_by_token.return_value = None
@@ -54,17 +40,10 @@ async def test_renew_token_raises_when_refresh_token_not_found(
     with pytest.raises(InvalidCredentialsException):
         await token_service.renew_token(refresh_token="refresh_token")
 
-    # then
-    mock_refresh_token_repository.find_by_token.assert_called_once()
-    mock_user_repository.find.assert_not_called()
-    mock_refresh_token_repository.save.assert_not_called()
-    mock_refresh_token_repository.update_all_tokens_expires_at.assert_not_called()
-
 
 async def test_renew_token_raises_when_refresh_token_is_expired(
     token_service: TokenService,
     mock_refresh_token_repository: AsyncMock,
-    mock_user_repository: AsyncMock,
     mock_refresh_token: Mock,
 ) -> None:
     # given
@@ -72,32 +51,5 @@ async def test_renew_token_raises_when_refresh_token_is_expired(
     mock_refresh_token.expires_at = datetime.now(UTC) - timedelta(days=1)
 
     # when
-    with pytest.raises(TokenExpiredException):
-        await token_service.renew_token(refresh_token=mock_refresh_token.token)
-
-    # then
-    mock_refresh_token_repository.find_by_token.assert_called_once()
-    mock_user_repository.find.assert_not_called()
-    mock_refresh_token_repository.save.assert_not_called()
-    mock_refresh_token_repository.update_all_tokens_expires_at.assert_not_called()
-
-
-async def test_renew_token_raises_when_refresh_token_is_not_latest(
-    token_service: TokenService,
-    mock_refresh_token_repository: AsyncMock,
-    mock_user_repository: AsyncMock,
-    mock_refresh_token: Mock,
-) -> None:
-    # given
-    mock_refresh_token_repository.find_by_token.return_value = mock_refresh_token
-    mock_refresh_token_repository.find_latest_token_by_user_id.return_value = "latest_token"
-
-    # when
     with pytest.raises(InvalidRefreshTokenException):
         await token_service.renew_token(refresh_token=mock_refresh_token.token)
-
-    # then
-    mock_refresh_token_repository.find_by_token.assert_called_once()
-    mock_refresh_token_repository.find_latest_token_by_user_id.return_value = "latest_token"
-    mock_refresh_token_repository.update_all_tokens_expires_at.assert_called_once()
-    mock_refresh_token_repository.save.assert_not_called()
